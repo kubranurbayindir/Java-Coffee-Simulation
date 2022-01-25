1b261644b1df2753289216e380f2435dab33c407
Kübra Nur BAYINDIR <83094285+kubranurbayindir@users.noreply.github.com>
2022-01-25 14:09:54 +0300

    Update CustomerCheckManager.java

--- coffeesimulator/src/Concreate/CustomerCheckManager.java
+++ coffeesimulator/src/Concreate/CustomerCheckManager.java
@@ -5,8 +5,13 @@ import Entities.Customer;
 public class CustomerCheckManager implements ICustomerCheckService {
 
 	@Override
-	public boolean CheckIfRealPerson(Customer customer) {
-		return true;
+	public boolean customerCheckIfEmpty(Customer customer) {
+		if (user != null && !user.getFirstName().isEmpty() && !user.getLastName().isEmpty()
+				&& !user.getDateOfBirth().isEmpty() && !user.getNationalityId().isEmpty()) {
+			return true;
+		}
+		System.out.println("These fields cannot be blank!");
+		return false;
 	}
 
 }
